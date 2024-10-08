e6e340ae4fbf30157a08f8678976f096dbf0e0f0
Oneil Achord <oneil.achord22@my.northampton.ac.uk>
2024-10-08 20:38:38 +0100

    updated css for nav and js for displaying nav

--- website/js/animations.js
+++ website/js/animations.js
@@ -2,13 +2,34 @@ function loader() {
     document.addEventListener("scroll", scrollToDivs);
 }
 
-function scrollToDivs() {
-    let hoursDiv = document.getElementById('hours');
-    let datesDiv = document.getElementById('dates');
+// *Future Implementation*
 
-    console.log("hours:", hoursDiv.offsetTop, "dates:", datesDiv.offsetTop)
+// function navBarAnimations() {
+//     const header = document.getElementById('header');
+//     const nav = document.getElementById('nav');
+
+//     if (header.offsetHeight < document.documentElement.scrollTop) {
+//         nav.style.display = "flex"
+//         nav.style.animationName = "drop-down-nav"
+//     } else {
+//         nav.style.animationName = "reverse-drop-down-nav"
+//         setTimeout(() => nav.style.display = "none", 450);
+//     }
+// }
+
+function scrollToDivs() {
+    const hoursDiv = document.getElementById('hours');
+    const datesDiv = document.getElementById('dates');
+    const header = document.getElementById('header');
+    const nav = document.getElementById('nav');
 
     let pageBottom = window.innerHeight + document.documentElement.scrollTop;
+
+    if (header.offsetHeight < document.documentElement.scrollTop) {
+        nav.style.display = "flex"
+    } else {
+        nav.style.display = "none"
+    }
 
     if (hoursDiv.offsetTop < pageBottom) {
         hoursDiv.classList.add("visible");
